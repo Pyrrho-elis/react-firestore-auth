ab43f922d1e1b33f290bfc5d28bd36f1f7bf4ad0
Pyrrho <67134966+Pyrrho-elis@users.noreply.github.com>
2023-07-27 09:07:10 +0000

    Update 3 files

--- src/firebse.js
+++ src/firebse.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCCliylac_enqBHpF6QSUMUOZm9-QRCEAU",
   authDomain: "dan-todo-94133.firebaseapp.com",
@@ -20,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
